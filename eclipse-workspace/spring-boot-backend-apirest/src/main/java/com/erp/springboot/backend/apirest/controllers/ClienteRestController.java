8d5606803c057dbd4e19c322ee1b8cbeffabac66
acaralps <caralpsabel@gmail.com>
2023-05-21 15:23:01 +0200

    daily_commit

--- eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/erp/springboot/backend/apirest/controllers/ClienteRestController.java
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/erp/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,9 +1,13 @@
-package com.erp.springboot.backend.apirest.controllers;
+ package com.erp.springboot.backend.apirest.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,8 +35,24 @@ public class ClienteRestController {
 		return clienteService.findAll();
 	}
 	@GetMapping("/clientes/{id}")
-	public Cliente show(@PathVariable Long id) {
-		return clienteService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		
+		Map<String, Object> response = new HashMap<>();
+		Cliente cliente = null;
+		try {
+			clienteService.findById(id);
+		}catch(DataAccessException e) {
+			response.put("mensaje", "Error al hacer la consulta a la BBDD");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+
+			return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(cliente == null) {
+			response.put("mensaje", "El Cliente ID: ".concat(id.toString().concat(" no existe en la BBDD")));
+			return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
 	}
 	
 	@PostMapping("/clientes")
