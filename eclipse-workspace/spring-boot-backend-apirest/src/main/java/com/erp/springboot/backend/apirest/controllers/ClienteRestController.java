05f036b2e7de915b87d8b8452da799642e216ead
acaralps <caralpsabel@gmail.com>
2023-06-13 23:53:38 +0200

    "error manejo"

--- eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/erp/springboot/backend/apirest/controllers/ClienteRestController.java
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/erp/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -56,9 +56,21 @@ public class ClienteRestController {
 	}
 	
 	@PostMapping("/clientes")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente create(@RequestBody Cliente cliente) {
-		return clienteService.save(cliente);
+	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+		Cliente clienteNew = null;
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			clienteNew = clienteService.save(cliente);
+		} catch(DataAccessException e) {
+			response.put("mesage", "Error to insert in DataBase");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mesage", "The client was create succesfull");
+		response.put("cliente", clienteNew);
+		return  new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/clientes/{id}")
